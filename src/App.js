4ebaca9a891947dad0a3b0f12db1d6e3bce5300b
jubair-codes <alpha01708@gmail.com>
2025-04-03 00:19:16 +0600

    hero-banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './images/logo.png';
 import './App.css';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 import data from './data/data.json';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header logo={logo}></Header>
+      <Hero></Hero>
       {
         data.map(course => <h2>{course.name}</h2>)
       }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = (props) => {
+    return (
+        <div className='w-100 hero-banner'>
+            <div className='w-100 h-100 hero-splash d-flex justify-content-center align-items-center'>
+                <h2 className='text-light'>Buy Freelancing Courses at best Prices from JubairCourses.app</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
